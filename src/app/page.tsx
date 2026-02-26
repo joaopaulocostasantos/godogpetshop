016c74294a12ad98ff90237a54912638c1f58b5e
joaopaulosanttos <contatosantosjp@gmail.com>
2026-02-26 10:45:05 -0300

    recovery

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,16 @@
-// components
-import { Container } from "@/components/container";
-import { Header } from "@/layout/header";
-import {
-  Hero,
-  Services,
-  Plans,
-  FeedBacks,
-  About,
-  Form,
-  Faq,
-} from "@/components/sections";
 import styles from "./page.module.scss";
-import { Footer } from "@/layout/footer";
+
+// components
+import { Container } from "@/components/Container";
+import { Header } from "@/layout/Header";
+import { Hero } from "@/components/Sections/Hero";
+import { Services } from "@/components/Sections/Services";
+import { Plans } from "@/components/Sections/Plans";
+import { FeedBacks } from "@/components/Sections/Feedback";
+import { About } from "@/components/Sections/About";
+import { Form } from "@/components/Sections/Form";
+import { Faq } from "@/components/Sections/Faq";
+import { Footer } from "@/layout/Footer";
 
 export default function Home() {
   return (
--- src/components/sections/index.ts
+++
@@ -1,7 +0,0 @@
-export { About } from "@/components/sections/about";
-export { Hero } from "@/components/sections/hero";
-export { Plans } from "@/components/sections/plans";
-export { Services } from "@/components/sections/services";
-export { FeedBacks } from "@/components/sections/Feedback";
-export { Form } from "@/components/sections/Form";
-export { Faq } from "@/components/sections/Faq";
